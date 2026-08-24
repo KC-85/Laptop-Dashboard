846c55740ea0d2843c780160f900853a9ea37f1d
Kristian Cross <etamin1985@yahoo.com>
2026-08-24 13:41:10 +0100

    Add Docstring for memory/stats.py

---
+++ src/laptop_dashboard/memory/stats.py
@@ -0,0 +1,5 @@
+"""
+This will show the memory stats of the system. It will show the total, used, free, and available memory in bytes.
+It will also show the memory usage percentage and the swap memory stats.
+The output will be in a human-readable format.
+"""
