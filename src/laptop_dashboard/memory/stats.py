2e723b3c339b708332a937ec672ef148b7aab6d2
Kristian Cross <etamin1985@yahoo.com>
2026-08-24 14:14:33 +0100

    Add classes & functions for memory & swap stats

--- src/laptop_dashboard/memory/stats.py
+++ src/laptop_dashboard/memory/stats.py
@@ -3,3 +3,50 @@ This will show the memory stats of the system. It will show the total, used, fre
 It will also show the memory usage percentage and the swap memory stats.
 The output will be in a human-readable format.
 """
+
+
+from dataclasses import dataclass
+
+import psutil
+
+
+@dataclass
+class MemoryStats:
+    total: int
+    used: int
+    free: int
+    available: int
+    percentage: float
+
+
+@dataclass
+class SwapStats:
+    total: int
+    used: int
+    free: int
+    available: int
+    percentage: float
+
+
+def get_memory_stats() -> MemoryStats:
+    mem = psutil.virtual_memory()
+
+    return MemoryStats(
+        total=mem.total,
+        used=mem.used,
+        free=mem.free,
+        available=mem.available,
+        percentage=mem.percent,
+    )
+
+
+def get_swap_stats() -> SwapStats:
+    swap = psutil.swap_memory()
+
+    return SwapStats(
+        total=swap.total,
+        used=swap.used,
+        free=swap.free,
+        available=swap.free,  # Swap does not have an 'available' attribute, using 'free' instead
+        percentage=swap.percent,
+    )
